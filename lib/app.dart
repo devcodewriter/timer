fe1bfca793cee70b320d609c44cae42f3240e586
devcodewriter <165512414+devcodewriter@users.noreply.github.com>
2025-08-17 00:27:33 +0000

    Implementing timer with bloc.

---
+++ lib/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'timer/timer.dart';
+
+class TimerApp extends StatelessWidget {
+  const TimerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Timer",
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Color.fromRGBO(72, 74, 126, 1),
+        ),
+      ),
+      home: TimerPage(),
+    );
+  }
+}
